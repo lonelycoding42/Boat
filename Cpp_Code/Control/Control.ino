03c0d013018a93d76f8fce700c59e94678e1eafc
shibei <2939633973@qq.com>
2024-04-24 12:16:49 +0800

    添加了两个拉杆的状态控制灯

--- Cpp_Code/Control/Control.ino
+++ Cpp_Code/Control/Control.ino
@@ -1,9 +1,14 @@
 #include <WiFi.h>
 #include <esp_now.h>
+#include <ESP32Servo.h>
+int l = 0;
 
 //摇杆引脚
 const int ana=32;   //舵机控制
+const int LED1 = 25; //舵机状态灯
+
 const int car_v=33; //电调控制
+const int LED2 = 26; //电调状态灯
 
 // 设置数据结构体
 typedef struct struct_message {
@@ -14,12 +19,17 @@ typedef struct struct_message {
 struct_message myData;
 
 // 接收设备的 MAC 地址
-uint8_t broadcastAddress[] = {0x08, 0xD1, 0xF9, 0xE7, 0x2C, 0xE0};
+uint8_t broadcastAddress[] = {0x08, 0xD1, 0xF9, 0xE7, 0x44, 0x28};
+                            //0x08, 0xD1, 0xF9, 0xE6, 0xCC, 0xEC
 
 void setup() {
+  Serial.begin(115200);
+
   //启用端口
   pinMode(ana,INPUT);
   pinMode(car_v,INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
 
   // 初始化 ESP-NOW(if->while)
   WiFi.mode(WIFI_STA);
@@ -44,18 +54,33 @@ void setup() {
   }
 
   //初始化舵机模块
-  myData.pos=135;
+  myData.pos=90;
   myData.velocity=0;
 }
 
 void loop() {
   //读取并处理摇杆数据
-  myData.pos=(double)analogRead(ana)*8/439+105.38;
+  myData.pos = analogRead(ana);
+  analogWrite(LED1, abs((myData.pos-1877)/16));
+  // Serial.println(myData.pos);
+
+  if(myData.pos<1880){
+    myData.pos= map(myData.pos,0,1880,160,75);
+  }else{
+    myData.pos= map(myData.pos,1880,4095,75,0);
+  }
+  //(double)analogRead(ana)*8/439+105.38;
+  // Serial.println(myData.pos);
+
+
   myData.velocity=analogRead(car_v);
-  myData.velocity=map(myData.velocity,0,4095,0,180);
+  // Serial.println(myData.velocity);
+  analogWrite(LED2, abs(myData.velocity-2322)/16);
+  myData.velocity=map(myData.velocity,4095,0,180,0);
+  Serial.println(myData.velocity);
 
   // 调试时使用
-  // Serial.println(myData.pos);
+  
   // Serial.println(myData.velocity);
 
   //发送数据
